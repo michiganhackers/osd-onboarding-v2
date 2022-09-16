15e8f51d90cfa19466b206eea9282b91de446ac8
Qiping Pan <60682078+ppppqp@users.noreply.github.com>
2022-09-15 20:19:51 -0400

    feat: REAL faster

--- welcome_rs/src/main.rs
+++ welcome_rs/src/main.rs
@@ -1,10 +1,20 @@
 fn fib(n: i32) -> i64 {
-    if n == 0 || n == 1 {
-        return n.into();
+    let mut cur: i64;
+    let mut prev: i64;
+    prev = 0;
+    cur = 1;
+    if n == 0 {
+        return 0;
     }
-    fib(n-2) + fib(n-1)
+    for _i in 0..n {
+        let temp: i64;
+        temp = cur;
+        cur += prev;
+        prev = temp;
+    }
+    return cur;
 }
-
+0 1 1 2 3 
 fn main() {
     const MH_LOGO_ASCII: &str = include_str!("mh_logo_ascii.txt");
     println!("{}", MH_LOGO_ASCII);

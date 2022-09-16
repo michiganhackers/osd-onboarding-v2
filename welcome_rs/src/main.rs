51692f350411c039424a613ec7d877db8dfe6d71
Qiping Pan <60682078+ppppqp@users.noreply.github.com>
2022-09-15 22:35:56 -0400

    feat: make it fast (#9)
    * feat: make it go faster
    
    * feat: add gitignore
    
    * feat: REAL faster
    
    * fix: unused
    
    * fix: newline at the end of gitignore

--- welcome_rs/src/main.rs
+++ welcome_rs/src/main.rs
@@ -1,10 +1,19 @@
 fn fib(n: i32) -> i64 {
-    if n == 0 || n == 1 {
-        return n.into();
+    let mut cur: i64;
+    let mut prev: i64;
+    prev = 0;
+    cur = 1;
+    if n == 0 {
+        return 0;
     }
-    fib(n-2) + fib(n-1)
+    for _i in 0..n {
+        let temp: i64;
+        temp = cur;
+        cur += prev;
+        prev = temp;
+    }
+    return cur;
 }
-
 fn main() {
     const MH_LOGO_ASCII: &str = include_str!("mh_logo_ascii.txt");
     println!("{}", MH_LOGO_ASCII);
@@ -13,7 +22,7 @@ fn main() {
 
     println!("Doing some math...");
     println!("fib(6) = {}", fib(6));
-    // Why is this so slow...
+    // Why is this so fast...
     // TODO: make it go faster
     println!("fib(42) = {}", fib(42));
 }

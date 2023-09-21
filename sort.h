cc4613f86f39b7b61c3d47005cf42441bbaec060
Brenden Forward <bforward@umich.edu>
2023-09-21 19:49:31 -0400

    style(cpp): fix styling in C++ files

--- sort.h
+++ sort.h
@@ -2,6 +2,15 @@
 #include <iostream>
 using namespace std;
 
-int is_sorted(int *arr, int n);
-void shuffle(int *arr, int n);
-void my_sort(int *arr, int n);
+int is_sorted(
+    int *
+        arr,
+    int n);
+void shuffle(
+    int *
+        arr,
+    int n);
+void my_sort(
+    int *
+        arr,
+    int n);
--- welcome.cpp
+++ welcome.cpp
@@ -1,15 +1,27 @@
 #include "sort.h"
 #include <iostream>
 
-int fib(int n) {
-    if (n == 0 || n == 1) {
-        return n + 1;
-    }
-    return fib(n - 2) + fib(n - 1);
+int fib(
+    int n) {
+if (n ==
+        0 ||
+    n ==
+        1) {
+return n +
+       1;
+}
+return fib(n -
+           2) +
+       fib(n -
+           1);
 }
 
-int main(int argc, char *argv[]) {
-    std::cout << " \
+int main(
+    int argc,
+    char *argv
+        []) {
+std::cout
+    << " \
                  .......            ......                  \n\
                 `kKXXXX0:          cKXXXXKx.                \n\
                 :NMMMMMWo         .xMMMMMMK:                \n\
@@ -35,25 +47,73 @@ int main(int argc, char *argv[]) {
                 :NMMMMMWo         .xMMMMMMK:                \n\
                 :XMMMMMWl         .dWMMMMMK,                \n\
                 .:lllll:.          .cllllc,                 \n"
-              << "\n";
-    std::cout << "Hello from Michigan Hackers!\n"
-              << "Learn more: https://www.youtube.com/watch?v=dQw4w9WgXcQ\n";
+    << "\n";
+std::cout
+    << "Hello from Michigan Hackers!\n"
+    << "Learn more: https://www.youtube.com/watch?v=dQw4w9WgXcQ\n";
 
-    std::cout << "Doing some math...\n";
-    // Why is this so slow...
-    // TODO: make it go faster
-    std::cout << "Input your first favorite number: \n";
-    int inputNumber1, inputNumber2;
-    std::cin >> inputNumber1;
-    std::cout << "Input your second favorite number: \n";
-    std::cin >> inputNumber2;
-    std::cout << "fib(" << inputNumber1 << ") = " << fib(inputNumber1) << std::endl;
-    std::cout << "fib(" << inputNumber2 << ") = " << fib(inputNumber2) << std::endl;
-    int arr[] = {5, 3, 9, 8, 3};
-    // Why is it printing weird things...
-    std::cout << "Sorting " << arr << "...\n";
-    my_sort(arr, 5);
-    std::cout << "Sorted: " << arr << "\n";
-    // Please delete the line below
-    std::cout << "Hello, this is Jackson!\n";
+std::cout
+    << "Doing some math...\n";
+// Why
+// is
+// this
+// so
+// slow...
+// TODO:
+// make
+// it go
+// faster
+std::cout
+    << "Input your first favorite number: \n";
+int inputNumber1,
+    inputNumber2;
+std::cin >>
+    inputNumber1;
+std::cout
+    << "Input your second favorite number: \n";
+std::cin >>
+    inputNumber2;
+std::cout
+    << "fib("
+    << inputNumber1
+    << ") = "
+    << fib(inputNumber1)
+    << std::
+           endl;
+std::cout
+    << "fib("
+    << inputNumber2
+    << ") = "
+    << fib(inputNumber2)
+    << std::
+           endl;
+int arr[] =
+    {5,
+     3,
+     9,
+     8,
+     3};
+// Why
+// is it
+// printing
+// weird
+// things...
+std::cout
+    << "Sorting "
+    << arr
+    << "...\n";
+my_sort(
+    arr,
+    5);
+std::cout
+    << "Sorted: "
+    << arr
+    << "\n";
+// Please
+// delete
+// the
+// line
+// below
+std::cout
+    << "Hello, this is Jackson!\n";
 }
